53a7a0d015db2c57e0db755a5ec7794964346448
UBC Student <b.wu102051@gmail.com>
2026-02-06 23:55:35 -0800

    - switched from local file fetching to s3 - i forgor

--- transport2055/src/data/maplayers.ts
+++ transport2055/src/data/maplayers.ts
@@ -1,46 +1,46 @@
-// const STORAGE_BASE_URL = import.meta.env.VITE_CLOUDFRONT_BASE_URL ?? ''
+const STORAGE_BASE_URL = import.meta.env.VITE_CLOUDFRONT_BASE_URL ?? ''
 
 export const TRANSIT_LAYERS = [
   {
     id: 'skytrain',
     // url: `${import.meta.env.BASE_URL}data/geojson/skytrain.geojson`,
-    url: `${import.meta.env.BASE_URL}data/geojson/skytrain/skytrain-260203-0.geojson`,
+    url: `${STORAGE_BASE_URL}/data/geojson/skytrain/skytrain-260203-0.geojson`,
     defaultColor: '#0055ff',
   },
   {
     id: 'rapidbus',
     // url: `${STORAGE_BASE_URL}/data/geojson/rapidbus/rapidbus-260201-0.geojson`,
-    url: `${import.meta.env.BASE_URL}data/geojson/rapidbus/rapidbus-260201-0.geojson`,
+    url: `${STORAGE_BASE_URL}/data/geojson/rapidbus/rapidbus-260201-0.geojson`,
     defaultColor: '#26bf1b',
   },
   {
     id: 'regionalrail',
     // url: `${import.meta.env.BASE_URL}data/geojson/rapidbus.geojson`,
-    url: `${import.meta.env.BASE_URL}data/geojson/regionalrail/regionalrail-260201-0.geojson`,
+    url: `${STORAGE_BASE_URL}/data/geojson/regionalrail/regionalrail-260201-0.geojson`,
     defaultColor: '#26bf1b',
   },
   {
     id: 'expressbus',
     // url: `${import.meta.env.BASE_URL}data/geojson/rapidbus.geojson`,
-    url: `${import.meta.env.BASE_URL}data/geojson/expressbus/expressbus-260204-0.geojson`,
+    url: `${STORAGE_BASE_URL}/data/geojson/expressbus/expressbus-260204-0.geojson`,
     defaultColor: '#26bf1b',
   },
   {
     id: 'tram',
     // url: `${import.meta.env.BASE_URL}data/geojson/rapidbus.geojson`,
-    url: `${import.meta.env.BASE_URL}data/geojson/landtrain/landtrain-260205-0.geojson`,
+    url: `${STORAGE_BASE_URL}/data/geojson/landtrain/landtrain-260205-0.geojson`,
     defaultColor: '#26bf1b',
   },
   {
     id: 'brt',
     // url: `${import.meta.env.BASE_URL}data/geojson/rapidbus.geojson`,
-    url: `${import.meta.env.BASE_URL}data/geojson/brt/brt-260206-0.geojson`,
+    url: `${STORAGE_BASE_URL}/data/geojson/brt/brt-260206-0.geojson`,
     defaultColor: '#26bf1b',
   },
   {
     id: 'gondola',
     // url: `${import.meta.env.BASE_URL}data/geojson/rapidbus.geojson`,
-    url: `${import.meta.env.BASE_URL}data/geojson/skybus/skybus-260206-0.geojson`,
+    url: `${STORAGE_BASE_URL}/data/geojson/skybus/skybus-260206-0.geojson`,
     defaultColor: '#26bf1b',
   }
 ]
