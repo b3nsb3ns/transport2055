1aca9901975eedfcca25c6c1e253b53860dc8bb3
UBC Student <b.wu102051@gmail.com>
2026-02-08 16:07:42 -0800

    - changed some lines in skytrain, bonus - changed file structure for markdown files

--- transport2055/src/lib/markdownLoader.ts
+++ transport2055/src/lib/markdownLoader.ts
@@ -1,23 +1,23 @@
 import { loadManifest } from './manifestLoader.ts'
 
-const STORAGE_BASE_URL = import.meta.env.VITE_CLOUDFRONT_BASE_URL ?? ''
+// const STORAGE_BASE_URL = import.meta.env.VITE_CLOUDFRONT_BASE_URL ?? ''
 
 const cache = new Map<string, Promise<string>>()
 
 export async function loadMarkdown(id: string): Promise<string> {
   const manifest = await loadManifest()
-  const filename = manifest[id]
+  const pathname = manifest[id]
 
-  if (!filename) {
+  if (!pathname) {
     throw new Error(`Unknown markdown id: ${id}`)
   }
 
   if (!cache.has(id)) {
-    const promise = fetch(`${STORAGE_BASE_URL}/data/content/${id}/${filename}`)
-    // const promise = fetch(`${import.meta.env.BASE_URL}data/content/${filename}`)
+    // const promise = fetch(`${STORAGE_BASE_URL}/data/content/${id}/${pathname}`)
+    const promise = fetch(`${import.meta.env.BASE_URL}data/content/${pathname}`)
       .then(res => {
         if (!res.ok) {
-          throw new Error(`Failed to load ${filename}`)
+          throw new Error(`Failed to load ${pathname}`)
         }
         return res.text()
       })
